16d6477036a95d7ee77c1593cdc23ff1af512c0c
Justin Lubin <jlubi333@gmail.com>
2021-01-27 10:46:06 -0800

    Move search inputs to separate file

---
+++ static/search-inputs.js
@@ -0,0 +1,371 @@
+(function(Search) {
+  "use strict";
+
+  Search.allInputs = [
+    [{
+      "data": 5,
+      "left": {
+        "data": 3,
+        "left": {
+          "data": 2,
+          "left": null,
+          "right": null
+        },
+        "right": {
+          "data": 4,
+          "left": null,
+          "right": null
+        }
+      },
+      "right": {
+        "data": 7,
+        "left": null,
+        "right": null
+      },
+    }, 4],
+
+    [{
+      "data": 8,
+      "left": {
+        "data": 3,
+        "left": {
+          "data": 1,
+          "left": null,
+          "right": null
+        },
+        "right": {
+          "data": 6,
+          "left": {
+            "data": 4,
+            "left": null,
+            "right": null
+          },
+          "right": {
+            "data": 7,
+            "left": null,
+            "right": null
+          }
+        }
+      },
+      "right": {
+        "data": 10,
+        "left": null,
+        "right": {
+          "data": 14,
+          "left": {
+            "data": 13,
+            "left": null,
+            "right": null
+          },
+          "right": null
+        }
+      },
+    }, 14],
+
+    [{
+      "data": 50,
+      "left": {
+        "data": 17,
+        "left": {
+          "data": 9,
+          "left": null,
+          "right": {
+            "data": 14,
+            "left": {
+              "data": 12,
+              "left": null,
+              "right": null
+            },
+            "right": null
+          },
+        },
+        "right": {
+          "data": 23,
+          "left": {
+            "data": 19,
+            "left": null,
+            "right": null
+          },
+          "right": null
+        }
+      },
+      "right": {
+        "data": 76,
+        "left": {
+          "data": 54,
+          "left": null,
+          "right": {
+            "data": 72,
+            "left": {
+              "data": 67,
+              "left": null,
+              "right": null
+            },
+            "right": null
+          },
+        },
+        "right": null
+      },
+    }, 76],
+
+    [{
+      "data": 7,
+      "left": {
+        "data": 5,
+        "left": {
+          "data": 3,
+          "left": {
+            "data": 1,
+            "left": null,
+            "right": null
+          },
+          "right": {
+            "data": 4,
+            "left": null,
+            "right": null
+          }
+        },
+        "right": {
+          "data": 6,
+          "left": null,
+          "right": null
+        }
+      },
+      "right": {
+        "data": 12,
+        "left": {
+          "data": 9,
+          "left": {
+            "data": 8,
+            "left": null,
+            "right": null
+          },
+          "right": {
+            "data": 10,
+            "left": null,
+            "right": null
+          },
+        },
+        "right": {
+          "data": 15,
+          "left": {
+            "data": 13,
+            "left": null,
+            "right": null
+          },
+          "right": {
+            "data": 17,
+            "left": null,
+            "right": null
+          },
+        },
+      },
+    }, 6],
+
+    [{
+      "data": 5,
+      "left": {
+        "data": 3,
+        "left": {
+          "data": 2,
+          "left": null,
+          "right": null
+        },
+        "right": {
+          "data": 4,
+          "left": null,
+          "right": null
+        }
+      },
+      "right": {
+        "data": 7,
+        "left": null,
+        "right": null
+      },
+    }, 7],
+
+    [{
+      "data": 8,
+      "left": {
+        "data": 3,
+        "left": {
+          "data": 1,
+          "left": null,
+          "right": null
+        },
+        "right": {
+          "data": 6,
+          "left": {
+            "data": 4,
+            "left": null,
+            "right": null
+          },
+          "right": {
+            "data": 7,
+            "left": null,
+            "right": null
+          }
+        }
+      },
+      "right": {
+        "data": 10,
+        "left": null,
+        "right": {
+          "data": 12,
+          "left": {
+            "data": 13,
+            "left": null,
+            "right": null
+          },
+          "right": null
+        }
+      },
+    }, 1],
+
+    [{
+      "data": 50,
+      "left": {
+        "data": 17,
+        "left": {
+          "data": 9,
+          "left": null,
+          "right": {
+            "data": 14,
+            "left": {
+              "data": 8,
+              "left": null,
+              "right": null
+            },
+            "right": null
+          },
+        },
+        "right": {
+          "data": 23,
+          "left": {
+            "data": 19,
+            "left": null,
+            "right": null
+          },
+          "right": null
+        }
+      },
+      "right": {
+        "data": 76,
+        "left": {
+          "data": 54,
+          "left": null,
+          "right": {
+            "data": 69,
+            "left": {
+              "data": 67,
+              "left": null,
+              "right": null
+            },
+            "right": null
+          },
+        },
+        "right": null
+      },
+    }, 69],
+
+    [{
+      "data": 7,
+      "left": {
+        "data": 5,
+        "left": {
+          "data": 3,
+          "left": {
+            "data": 1,
+            "left": null,
+            "right": null
+          },
+          "right": {
+            "data": 4,
+            "left": null,
+            "right": null
+          }
+        },
+        "right": {
+          "data": 6,
+          "left": null,
+          "right": null
+        }
+      },
+      "right": {
+        "data": 12,
+        "left": {
+          "data": 9,
+          "left": {
+            "data": 8,
+            "left": null,
+            "right": null
+          },
+          "right": {
+            "data": 13,
+            "left": null,
+            "right": null
+          },
+        },
+        "right": {
+          "data": 15,
+          "left": {
+            "data": 10,
+            "left": null,
+            "right": null
+          },
+          "right": {
+            "data": 17,
+            "left": null,
+            "right": null
+          },
+        },
+      },
+    }, 15]
+  ]
+
+  Search.testInputs = [
+    [{
+      "data": 19,
+      "left": {
+        "data": 7,
+        "left": {
+          "data": 4,
+          "left": null,
+          "right": null
+        },
+        "right": {
+          "data": 13,
+          "left": null,
+          "right": {
+            "data": 15,
+            "left": null,
+            "right": null
+          }
+        }
+      },
+      "right": {
+        "data": 34,
+        "left": {
+          "data": 4,
+          "left": {
+            "data": 23,
+            "left": null,
+            "right": null
+          },
+          "right": null
+        },
+        "right": null
+      },
+      "right": {
+        "data": 56,
+        "left": null,
+        "right": {
+          "data": 71,
+          "left": null,
+          "right": null
+        }
+      }
+    }, 13]
+  ]
+
+})(window.Search = window.Search || {});
